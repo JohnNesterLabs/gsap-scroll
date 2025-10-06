44658bef81b4d04e815c009a6041ed8c5992bbdd
JohnNesterLabs <john.horo@nesterlabs.com>
2025-10-06 14:45:01 +0530

    Add new layout

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScrollSyncModel from './components/scroll-sections/ScrollSyncModel/ScrollSyncModel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ScrollSyncModel/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
