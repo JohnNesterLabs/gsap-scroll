fd557f736e844cbde11fdfc4a1eb1d3aca91db07
pooja-nest <pooja@nesterlabs.com>
2025-10-06 14:20:12 +0530

    removed the error

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ScrollSyncModel from './components/ScrollSyncModel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ScrollSyncModel/>
+    <App />
   </React.StrictMode>
 );
 
